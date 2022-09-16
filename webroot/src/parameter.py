d2ced5e846f64e8658b21f7576e088edfe5407f1
zhixiaoni <zhixiaoni0@126.com>
2022-09-16 19:35:58 +0800

    1

--- webroot/src/parameter.py
+++ webroot/src/parameter.py
@@ -1,8 +1,25 @@
 import os
+import argparse
 
-
+# path
 webroot_path = os.path.abspath("..")
 src_path = os.getcwd()
 CGI_WebServer_path = os.path.abspath(r"../..")
 log_path = os.path.join(webroot_path,r"log")
+
+# net
+# 参数解析器
+parser = argparse.ArgumentParser()
+# 添加参数
+parser.add_argument('--ip', default = "127.0.0.1")
+parser.add_argument('-p','--port', default = 12141)
+# 解析
+args = parser.parse_args()
+# 参数
+ip = args.ip
+port = args.port
+
+
+
+
 
--- webroot/src/webServer.py
+++ webroot/src/webServer.py
@@ -1,4 +1,12 @@
-
+import socket
+import parameter
+import log
 
 def  WebServer():
-    print("start webServer")
+    #tcp
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    #udp socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    
+    # setsockopt
+    # 绑定
+    server.bind((parameter.ip, parameter.port))
