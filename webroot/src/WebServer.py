4a17a7665b654246107ebb7ee48c7a1152076232
zhixiaoni <zhixiaoni0@126.com>
2022-09-26 13:42:43 +0800

    change webServer.py into WebServer.py

---
+++ webroot/src/WebServer.py
@@ -0,0 +1,33 @@
+import socket
+import parameter
+from WorkData import WorkData
+from log import MyLog
+
+def  WebServer():
+    mylog = MyLog()
+    #tcp
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    #udp socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+
+    mylog.LogInfo("start tcp socket")
+    
+    # setsockopt 超时断连等设置
+    # server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # 绑定
+    server.bind((parameter.ip, int(parameter.port)))
+    
+    # 缓冲队列中的最大数目
+    server.listen(parameter.maxWaiting)
+    
+    while True:
+        
+        # 线程控制 最大数目 parameter.MaxConnection
+        if len(WorkData.connectingThread) < parameter.maxConnection:
+            # 接受
+            newsocket, client_addr = server.accept()
+            # 创建新线程处理连接
+            WorkData.connectingThread.append(WorkData(newsocket, client_addr, mylog))    #加入队列
+            WorkData.connectingThread[-1].setDaemon(True)   #设为守护进程
+            WorkData.connectingThread[-1].start()   #开始
+        
